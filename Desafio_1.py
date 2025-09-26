feefbe7e355d1aedae362cbccbd55609c273ccde
Camila-Fleitas <cami.fleitas.0@gmail.com>
2025-09-26 16:04:04 -0300

    Add files via upload

---
+++ Desafio_1.py
@@ -0,0 +1,10 @@
+#Suma de Dos Números: Crear un programa que sume dos números predefinidos y muestre el resultado.
+#Descripción:
+#Define dos variables con números específicos asignados por ti.
+#Suma esos dos números y almacena el resultado en una tercera variable.
+#Muestra el resultado de la suma.
+
+a = 2
+b = 8
+suma = a + b
+print(suma)
