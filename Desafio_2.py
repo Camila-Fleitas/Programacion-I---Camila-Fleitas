585aebc8c59508182d5f4da0a37eb899ea1afcf2
Camila-Fleitas <cami.fleitas.0@gmail.com>
2025-09-26 16:39:26 -0300

    Add files via upload

---
+++ Desafio_2.py
@@ -0,0 +1,16 @@
+# Calcular el Área de un Rectángulo con Presentación:
+# Escribir un programa que, utilizando valores predefinidos, calcule el área de un rectángulo y
+# presente los resultados de manera amigable utilizando texto.
+#Descripción:
+# Define dos variables numéricas que representen el ancho y el largo de un rectángulo.
+# Define variables de tipo string que contengan texto explicativo sobre lo que hace el programa.
+# Calcula el área del rectángulo (ancho x largo).
+# Utiliza tanto variables numéricas como de texto para presentar el resultado de una manera que
+# sea fácil de entender para alguien que no está viendo el código.
+
+a = 3
+l = 9
+
+print ("Este programa calcula el área de un rectángulo")
+area = a * l
+print("El área del rectángulo es:", area)
