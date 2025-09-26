51a8c3189a9ec27cc8c3183c65c344d04d938f93
Camila-Fleitas <cami.fleitas.0@gmail.com>
2025-09-26 18:16:02 -0300

    Add files via upload

---
+++ 2_5_EntradaySalidadeDatos_Desafio1.py
@@ -0,0 +1,7 @@
+# Desafío 1: Saludo Personalizado: Escriba un programa que pida al usuario su nombre y su ciudad de origen,
+# luego imprima un saludo personalizado con esa información.
+
+nombre = input("Ingrese su nombre: ")
+ciudad = input ("Ingrese su ciudad de origen: ")
+
+print (f"Su nombre es {nombre} y su ciudad de origen es {ciudad}")
